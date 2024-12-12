2fdc30cf5999d786b5bb40336977fd22f9199f15
Athanasia Farmaki <farmaki@kth.se>
2024-12-12 14:13:54 +0100

    Add crypto api call

---
+++ utils.py
@@ -0,0 +1,12 @@
+import requests
+
+def trigger_request(url:str, params:dict):
+    response = requests.get(url, params)
+    if response.status_code == 200:
+        # Extract the JSON content from the response
+        data = response.json()
+    else:
+        print("Failed to retrieve data. Status Code:", response.status_code)
+        raise requests.exceptions.RequestException(response.status_code)
+
+    return data
