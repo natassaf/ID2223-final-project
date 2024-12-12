e60714ce9fa9d523010fb79a49b98720cb0d00bb
michailroussos <michael_roussos@yahoo.com>
2024-12-12 15:49:37 +0100

    got the last date in hopsworks

--- utils.py
+++ utils.py
@@ -1,4 +1,19 @@
+import os
+import datetime
+import time
 import requests
+import pandas as pd
+import json
+from geopy.geocoders import Nominatim
+import matplotlib.pyplot as plt
+from matplotlib.patches import Patch
+from matplotlib.ticker import MultipleLocator
+import openmeteo_requests
+import requests_cache
+from retry_requests import retry
+import hopsworks
+import hsfs
+from pathlib import Path
 
 def trigger_request(url:str, params:dict):
     response = requests.get(url, params)
@@ -10,3 +25,11 @@ def trigger_request(url:str, params:dict):
         raise requests.exceptions.RequestException(response.status_code)
 
     return data
+
+
+def secrets_api(proj):
+    host = "c.app.hopsworks.ai"
+    api_key = os.environ.get('HOPSWORKS_API_KEY')
+    print(f"api_key {api_key}")
+    conn = hopsworks.connection(host=host, project=proj, api_key_value=api_key)
+    return conn.get_secrets_api()
